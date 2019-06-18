0af65546e4a81fb49ebe85bedea127202745dd4a
YeonhaPark <yonaprisca@gmail.com>
2019-06-18 22:58:54 +0900

    second commit

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Yona\'s 프론트엔드 블로그',
-  subtitle: '개발 공부하면서 중요한 내용들을 정리하고 있습니다.',
+  url: 'https://yeonhapark.github.io/',
+  title: '행복하게 코딩하기',
+  subtitle: '개발 공부하면서 중요한 내용들을 정리하고 공유할 예정입니다',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,7 +25,7 @@ module.exports = {
   author: {
     name: 'Park Yeonha',
     photo: '/profile.jpg',
-    bio: '자바스크립트, 리액트 JS 그리고 Responsive Design를 좋아하고 공부하는 프론트엔드 엔지니어',
+    bio: 'I love JavaScript, React and Responsive Design!',
     contacts: {
       email: '#',
       telegram: '#',
