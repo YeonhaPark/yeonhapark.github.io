c0d338077295728cdc06db0f0bb8647cab3aa9e6
YeonhaPark <yonaprisca@gmail.com>
2021-01-16 23:32:53 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9c15c47c25d7948fbe1f.js"
+    "url": "webpack-runtime-f8a91e56509fe8c0211a.js"
   },
   {
     "url": "styles.f53b834904fdad32adc8.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9226f75159b28925dd5e9747cc7c986f"
+    "revision": "17703981612c190f894237fd64198a22"
   },
   {
     "url": "manifest.json",
